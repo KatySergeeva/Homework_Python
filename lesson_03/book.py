9aa5bdcc255b42b085440056d7b227f3e1e80b10
KatySergeeva <ekaterinasergeeva39789@gmail.com>
2025-06-07 21:09:25 +0400

    first commit

---
+++ lesson_03/book.py
@@ -0,0 +1,12 @@
+class Book:
+
+    def __init__(self, name, autor):
+        self.name = name
+        self.autor = autor
+    
+    def get_name(self):
+        return self.name
+
+    def get_autor(self):
+        return self.autor
+        
