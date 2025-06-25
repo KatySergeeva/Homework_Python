d8e17e93468aed13c1c5752845ee86e73c6df1f4
KatySergeeva <ekaterinasergeeva39789@gmail.com>
2025-06-25 20:08:01 +0400

    Homework 5/1

--- 05_lesson/lesson05_task2.py
+++ 05_lesson/lesson05_task2.py
@@ -1,15 +1,14 @@
-from time import sleep 
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
 driver = webdriver.Chrome()
 
-driver.get("http://uitestingplayground.com/classattr")
-sleep (5)
+driver.get("http://uitestingplayground.com/dynamicid")
+sleep(5)
 
-button_input = driver.find_element(By.XPATH, "//button[contains(concat(' ', normalize-space(@class), ' '), ' btn-primary ')]")
-button_input.click()
+button = driver.find_element(By.CLASS_NAME, 'btn-primary')
+button.click()
 
 sleep(5)
 driver.quit()
