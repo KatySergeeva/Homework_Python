60e0455473c6c10b843a9469a907ff7913cf353d
KatySergeeva <ekaterinasergeeva39789@gmail.com>
2025-07-27 11:13:42 +0400

    Homework 08/1

--- 08_lesson/test_yougile.py
+++ 08_lesson/test_yougile.py
@@ -6,7 +6,7 @@ id = '3352c644-97d1-4bc6-8dea-1557d08230f6'
 
 
 # Позитив_получение списка проектов
-def test_positive_get_list_progect():
+def test_positive_get_list_project():
     headers = {
         'Authorization': f'Bearer {key}',
         'Content-Type': 'application/json'
@@ -16,7 +16,7 @@ def test_positive_get_list_progect():
 
 
 # Негатив_получение списка проектов с пустым значением ключа
-def test_negative_get_list_progect():
+def test_negative_get_list_project():
     headers = {
         'Authorization': '',
         'Content-Type': 'application/json'
@@ -40,7 +40,7 @@ def test_positive_change_name():
 
 
 # Негатив_пустое значение наименования проекта
-def test_negitive_change_name():
+def test_negative_change_name():
     headers = {
         'Authorization': f'Bearer {key}',
         'Content-Type': 'application/json'
@@ -54,7 +54,7 @@ def test_negitive_change_name():
 
 
 # Негатив_изменение наименования проекта с неправильным токеном
-def test_negitive_incorrect_token():
+def test_negative_incorrect_token():
     headers = {
         'Authorization': f'Bearer H6HngIA816fpIhY7tBvWx/it3YbVvEt/33Sk8afA39MCR9a',
         'Content-Type': 'application/json'
@@ -68,20 +68,46 @@ def test_negitive_incorrect_token():
 
 
 # Позитив_получение проекта по ID
-def test_positive_get_progect_ID():
+def test_positive_get_project_ID():
     headers = {
         'Authorization': f'Bearer {key}',
         'Content-Type': 'application/json'
     }
-    resp = requests.put(f'{base_url}/api-v2/projects/{id}', headers=headers)
+    resp = requests.get(f'{base_url}/api-v2/projects/{id}', headers=headers)
     assert resp.status_code == 200
 
 
-# Негатив_получение проекта без ID
-def test_negative_get_progect():
+# Негатив_получение проекта по некорректному ID
+def test_negative_get_project():
     headers = {
         'Authorization': f'Bearer {key}',
         'Content-Type': 'application/json'
     }
-    resp = requests.put(f'{base_url}/api-v2/projects/', headers=headers)
+    resp = requests.get(f'{base_url}/api-v2/projects/1234c567-89d1-0bc6-8dea-1112d13140f6', headers=headers)
     assert resp.status_code == 404
+
+
+# Позитив_создание нового проекта
+def test_positive_create_project():
+    headers = {
+        'Authorization': f'Bearer {key}',
+        'Content-Type': 'application/json'
+    }
+    body = {
+        "title": "My new project"
+    }
+    resp = requests.post(f'{base_url}/api-v2/projects', headers=headers, json=body)
+    assert resp.status_code == 201
+
+
+# Негатив_создание нового проекта с пустым значением наименования
+def test_negative_no_name_project():
+    headers = {
+        'Authorization': f'Bearer {key}',
+        'Content-Type': 'application/json'
+    }
+    body = {
+        "title": None
+    }
+    resp = requests.post(f'{base_url}/api-v2/projects', headers=headers, json=body)
+    assert resp.status_code == 400
